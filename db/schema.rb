30a80e2e0ee578c1f3778f473f22cf1f32178f6c
Maciej Borosiewicz <m.borosiewicz@gmail.com>
2012-11-06 23:04:46 +0100

    Clear schema.rb, get rid off deleted migration

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20121106121514) do
 
-  create_table "incomes", :force => true do |t|
-    t.string   "source"
-    t.decimal  "amount"
-    t.decimal  "tax"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
@@ -37,6 +29,5 @@ ActiveRecord::Schema.define(:version => 20121106121514) do
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
