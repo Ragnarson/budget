19a4a63c50bd8360a85c4ae71f620232ad10d523
Marcin Brzeziński <marcin.brzezinski.inf@gmail.com>
2012-11-07 22:52:34 +0100

    change name of view, index doesnt exist

--- test/functional/wallets_controller_test.rb
+++ test/functional/wallets_controller_test.rb
@@ -2,27 +2,27 @@ require 'test_helper'
 
 class WalletsControllerTest < ActionController::TestCase
 
-  test "should get index" do
-    get :index
+  test "should get new" do
+    get :new
     assert_response :success
     assert_not_nil assigns(:wallet)
   end
 
   test "should be form on page" do
-    get :index
+    get :new
     assert_select 'form' do
       assert_select 'input#wallet_name'
       assert_select 'input[TYPE=submit]'
     end
   end
 
-  test "should create budget and redirect to index with notice" do
+  test "should create budget and redirect to new with notice" do
     post :create, :wallet => { :name=> 'Some title'}
     assert_redirected_to :new_budget
     assert_equal 'Your budget was added successfully', flash[:notice]
   end
 
-  test "should redirect to index with error" do
+  test "should redirect to new with error" do
     post :create, :wallet => { :name=> ''}
     assert_redirected_to :new_budget
     assert_equal 'Budget could not be empty', flash[:error]
