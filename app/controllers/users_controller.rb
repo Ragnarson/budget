3fc4eeb9c21a7e8c75d25af3bc1cbe738239347a
Sebastian Wesołowski <seba.wesol@gmail.com>
2012-11-07 23:52:18 +0100

    Add user form + header top links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
 
   # GET users
-
   def index
     @users = User.all
 
@@ -10,14 +9,25 @@ class UsersController < ApplicationController
     end
   end
 
-
   # GET users/new
-
   def new
     @user = User.new
 
     respond_to do |format|
       format.html # new.html.erb
+    end
+  end
+
+  # POST users
+  def create
+    @user = User.new(params[:user])
+
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+      else
+        format.html { render action: "new" }
+      end
     end
   end
 end
