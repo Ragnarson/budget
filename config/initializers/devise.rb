46115632c945278d61dfdae3fab7eccf17b23b25
Karolina Kruk <karolina.anna.kruk@gmail.com>
2012-11-07 19:59:23 +0100

    Fixed small bug in configuration for logging by google

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -204,7 +204,7 @@ Devise.setup do |config|
 
   OmniAuth.config.full_host = "http://budget.shellyapp.com"
   require "omniauth-google-oauth2"
-  config.omniauth :google_oauth2, "http://84144236811.apps.googleusercontent.com", "w4D1PU08qZg_5Lnkez13Re99", { access_type: "offline", approval_prompt: "" }
+  config.omniauth :google_oauth2, "84144236811.apps.googleusercontent.com", "w4D1PU08qZg_5Lnkez13Re99", { access_type: "offline", approval_prompt: "" }
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
