7f4de019f2c7a0b9c7a78214e7684d9e821e6246
Maciej Małecki <smt116@gmail.com>
2012-11-07 22:14:10 +0100

    Remove unnecessary response block from controller and improve expense form layout

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -6,12 +6,10 @@ class ExpensesController < ApplicationController
   def create
     @expense = Expense.new(params[:expense])
 
-    respond_to do |format|
-      if @expense.valid?
-        format.html { redirect_to new_expense_path, notice: 'Expense was successfully created.' }
-      else
-        format.html { render action: "new" }
-      end
+    if @expense.valid?
+      redirect_to new_expense_path, notice: 'Expense was successfully created.'
+    else
+      render action: "new"
     end
   end
 end
