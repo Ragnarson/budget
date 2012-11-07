5010e9c7eebb4aae52144c23bcd7037514769436
Karolina Kruk <karolina.anna.kruk@gmail.com>
2012-11-07 20:09:22 +0100

    Added Omniauth callback in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Budget::Application.routes.draw do
   root to: 'Home#index'
 
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   match '/new_budget' => 'wallets#index', :as => :new_budget
   match '/create_budget' => 'wallets#create', :as => :create_budget, :via=> :post
