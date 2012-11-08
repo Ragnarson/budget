9fe64927e491ed3c8ab51b0f5f14e8b122d7a196
Maciej Borosiewicz <m.borosiewicz@gmail.com>
2012-11-08 22:19:56 +0100

    Redirect to Wallets#new when authentication succeeded

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    if user_signed_in?
+      redirect_to new_budget_path, notice: "Successfully authenticated! Now please create your first budget."
+    end
   end
 end
