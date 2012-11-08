5b497abd9fd7298df301160ee2e4b2649e36e862
Marcin Brzeziński <marcin.brzezinski.inf@gmail.com>
2012-11-08 18:49:39 +0100

    Missing declaration. Expense have also many wallets inside assignment table.

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,6 +2,7 @@ class Expense < ActiveRecord::Base
   attr_accessible :name, :amount
   
   has_many :assignments
+  has_many :wallets, through: :assignments
 
   validates :name, :presence => true, :length => { in: 3..128 }
   validates :amount, :presence => true, 
