fc2a399ac066cba02d17b80bb8bc552529931faa
Maciej Małecki <smt116@gmail.com>
2012-11-08 20:48:22 +0100

    Corrected tests for expense

--- test/functional/expenses_controller_test.rb
+++ test/functional/expenses_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class ExpensesControllerTest < ActionController::TestCase
-
   test "test form is visible on expense page" do
+    sign_in User.first
     get :new
     assert_select 'form' do
       assert_select 'input#expense_name'
@@ -25,13 +25,28 @@ class ExpensesControllerTest < ActionController::TestCase
     assert_equal Expense.new(:name => 'Milk', :amount => 3.15).valid?, true
   end
 
+  test "should save valid expense to database" do
+    assert_equal Expense.new(:name => 'My new SSD', :amount => 100.50).save, true
+    assert_equal Expense.new(:name => 'Milk', :amount => 3).save, true
+  end
+
+  test "should not save invalid expense to database" do
+    assert_equal Expense.new(:name => 'a').save, false
+    assert_equal Expense.new(:name => 'My new SSD', :amount => 0).save, false
+    assert_equal Expense.new(:amount => 10).save, false
+    assert_equal Expense.new(:name => 'My new SSD', :amount => -1).save, false
+    assert_equal Expense.new(:name => 'My new SSD', :amount => 0.555).save, false
+  end
+
   test "should redirect to new with notice" do
-    post :create, :expense => { :name => 'New SSD', :amount => 100.501 }
+    sign_in User.first
+    post :create, :expense => { :name => 'New SSD', :amount => 100.50 }
     assert_redirected_to :new_expense
     assert_equal 'Expense was successfully created.', flash[:notice]
   end
 
   test "should render new template" do
+    sign_in User.first
     post :create, :expense => {}
     assert_template :new
     post :create, :expense => { :name => 'Rt' }
