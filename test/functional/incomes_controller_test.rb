9f4bc1f0593907c67559830709194eca62c0d989
Dawid Milewski <dawidmilewski89@gmail.com>
2012-11-08 12:31:36 +0100

    Added some tests to income controller

---
+++ test/functional/incomes_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class IncomesControllerTest < ActionController::TestCase
+
+  test "should generate proper form on new income page" do
+    get :new
+    assert_select 'form' do
+      assert_select 'input#income_source'
+      assert_select 'input#income_amount'
+      assert_select 'input[TYPE=submit]'
+    end
+  end
+  
+  test "should render new if income amount is not valid" do
+    post :create, :income => { :source => 'source', :amount => 'zero' }
+    assert_template 'new'
+  end
+
+  test "should redirect to new income and notify about creation if source and amount are valid" do
+    post:create, :income => { :source => 'source', :amount => 200 }
+    assert_redirected_to new_income_path
+    assert_equal 'Income has been succesfully created', flash[:notice]
+  end
+end
