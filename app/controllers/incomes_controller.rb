51b69f95008f4115651c1e00f00032380acd88ad
Maciej Małecki <smt116@gmail.com>
2012-11-08 20:32:41 +0100

    Save object to database when user will add new expense or income.

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -6,7 +6,7 @@ class IncomesController < ApplicationController
 
   def create 
     @income = Income.new(params[:income])
-    if @income.valid?
+    if @income.save
       redirect_to new_income_path, notice: 'Income has been succesfully created'
     else
       render action: "new"
