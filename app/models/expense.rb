a21ec205d373379faeecb89eba9d684a9a9ce5ba
Maciej Małecki <smt116@gmail.com>
2012-11-09 22:13:45 +0100

    New hash syntax for expenses

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,8 +3,8 @@ class Expense < ActiveRecord::Base
   
   belongs_to :wallet
 
-  validates :name, :presence => true, :length => { in: 3..128 }
-  validates :amount, :presence => true, 
-    :format => { :with => /^\d+??(?:\.\d{0,2})?$/, :message => 'must be valid price' }, 
-    :numericality => { :greater_than => 0, :decimal => true}
+  validates :name, length: { in: 3..128 }
+  validates :amount,
+    format: { with: /^\d+??(?:\.\d{0,2})?$/, message: 'must be valid price' }, 
+    numericality: { :greater_than => 0 }
 end
