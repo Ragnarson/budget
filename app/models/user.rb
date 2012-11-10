cff980c0a721d482e1afa2fa905e1d3cabffa34e
Karolina Kruk <karolina.anna.kruk@gmail.com>
2012-11-10 20:07:12 +0100

    In user model change 'where' and 'create' for 'where' with 'first_or_create'

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,9 @@ class User < ActiveRecord::Base
 
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
     data = access_token.info
-    user = User.where(:email => data["email"]).first
+    user = User.where(:email => data["email"]).first_or_create(email: data["email"],
+                                                               password: Devise.friendly_token[0,20])
 
-    unless user
-      user = User.create(email: data["email"],
-                         password: Devise.friendly_token[0,20]
-      )
-    end
     user
   end
 
