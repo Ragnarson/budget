b88a4b1d7cfbfd25faa85e2dabe03aa15dca7602
Maciej Borosiewicz <m.borosiewicz@gmail.com>
2012-11-11 13:16:25 +0100

    I guess we can omit user variable in that case

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,8 @@ class User < ActiveRecord::Base
 
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
     data = access_token.info
-    user = User.where(:email => data["email"]).first_or_create(email: data["email"],
-                                                               password: Devise.friendly_token[0,20])
-
-    user
+    User.where(:email => data["email"]).first_or_create(email: data["email"],
+                                                        password: Devise.friendly_token[0,20])
   end
 
 end
