16594cd53e86bd13c4b846b462ae69088ca1bf95
Sebastian Wesołowski <seba.wesol@gmail.com>
2012-11-11 17:12:41 +0100

    Db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121109154438) do
+ActiveRecord::Schema.define(:version => 20121111091628) do
 
   create_table "expenses", :force => true do |t|
     t.string   "name",                                                       :null => false
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(:version => 20121109154438) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => ""
+    t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
