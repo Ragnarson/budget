dd1e0263986f277dc809a7d94da86284fd8cb614
Sebastian Wesołowski <seba.wesol@gmail.com>
2012-11-11 18:59:11 +0100

    Styles fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,7 @@
 class UsersController < ApplicationController
 
-  # GET users
   def index
     @users = User.where(:users => {:invited_by => current_user.id})
-
     respond_to do |format|
       format.html # index.html.erb
     end
@@ -12,35 +10,26 @@ class UsersController < ApplicationController
   # GET users/new
   def new
     @user = User.new
-
     respond_to do |format|
       format.html # new.html.erb
     end
   end
 
-  # POST users
   def create
     @user = User.new(params[:user])
     @user.password = User.generate_password
     @user.invited_by = current_user.id
-
-    respond_to do |format|
-      if @user.save
-        UserMailer.invite_email(@user).deliver
-        format.html { redirect_to users_path, notice: 'User was successfully created.' }
-      else
-        format.html { render action: "new" }
-      end
+    if @user.save
+      UserMailer.invite_email(@user).deliver
+      redirect_to users_path, notice: 'User was successfully created.'
+    else
+      render action: "new"
     end
   end
 
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-
-    respond_to do |format|
-      format.html { redirect_to users_path, notice: 'User was successfully deleted' }
-      format.json { head :no_content }
-    end
+    redirect_to users_path, notice: 'User was successfully deleted' 
   end
 end
