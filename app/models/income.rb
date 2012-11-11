ce7a5711efca305506907b8c9a7b7a4f04cb5c4e
Dawid Milewski <dawidmilewski89@gmail.com>
2012-11-11 19:03:17 +0100

    Incomes view and model refactorisation to meet standards

--- app/models/income.rb
+++ app/models/income.rb
@@ -1,8 +1,7 @@
 class Income < ActiveRecord::Base
   attr_accessible :source, :amount, :tax
 
-  validates :source, presence: true, length: { in: 1..128 }
-  validates :amount, presence: true, 
-      numericality: { greater_than: 0}
+  validates :source, length: { in: 1..128 }
+  validates :amount, numericality: { greater_than: 0}
   validates :tax, inclusion: { in: [18, 23, nil] }
 end
