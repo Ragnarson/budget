4303a764d8f56f4970339260a7a53c4abfd44e36
Dawid Milewski <dawidmilewski89@gmail.com>
2012-11-12 14:21:40 +0100

    Fixed obvious mistake in migration, sorry

--- db/migrate/20121112120106_set_income_user_not_null.rb
+++ db/migrate/20121112120106_set_income_user_not_null.rb
@@ -1,5 +1,5 @@
 class SetIncomeUserNotNull < ActiveRecord::Migration
   def change
-    change_table :incomes, :user_id, :integer, null: false
+    change_column :incomes, :user_id, :integer, null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121112102137) do
+ActiveRecord::Schema.define(:version => 20121112120106) do
 
   create_table "expenses", :force => true do |t|
     t.string   "name",                                                       :null => false
@@ -22,12 +22,12 @@ ActiveRecord::Schema.define(:version => 20121112102137) do
   end
 
   create_table "incomes", :force => true do |t|
-    t.string   "source",                                   :null => false
-    t.decimal  "amount",     :precision => 8, :scale => 2
-    t.decimal  "tax",        :precision => 2, :scale => 0
-    t.datetime "created_at",                               :null => false
-    t.datetime "updated_at",                               :null => false
-    t.integer  "user_id"
+    t.string   "source",                                                :null => false
+    t.decimal  "amount",                  :precision => 8, :scale => 2
+    t.integer  "tax",        :limit => 2
+    t.datetime "created_at",                                            :null => false
+    t.datetime "updated_at",                                            :null => false
+    t.integer  "user_id",                                               :null => false
     t.integer  "wallet_id"
   end
 
