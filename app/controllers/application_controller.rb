3de3f9dc5e901846f57c199fae81a69a3fdda467
Rafath Khan <rafath@zaraz.pl>
2012-11-19 06:14:36 +0100

    resolving email issue on member invitation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,7 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options(options={})
-    logger.debug "default_url_options is passed options: #{options.inspect}\n"
-    {:locale => I18n.locale}
+    {locale: I18n.locale}
   end
 
   def get_actual_balance
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,9 +2,9 @@ class UserMailer < ActionMailer::Base
   default from: "no-reply@shellyapp.com"
 
   def invite_email(params)
-    @user = params["email"]
-    @url = params["url"]
-    @current_user_email = params["current_user_email"]
-    mail(:to => params["email"], :subject => "Welcome to Budget application")
+    @user = params[:email]
+    @url = params[:url]
+    @current_user_email = params[:current_user_email]
+    mail(to: @user, subject: "Welcome to Budget application")
   end
 end
