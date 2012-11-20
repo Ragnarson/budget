8c9bc18ecba1272bb1e25286429dac2b7955924c
Karolina Kruk <karolina.anna.kruk@gmail.com>
2012-11-20 18:33:08 +0100

    Added layouts tests to missing methods

--- test/functional/layout_tests.rb
+++ test/functional/layout_tests.rb
@@ -1,9 +1,10 @@
 module LayoutTests
   extend ActiveSupport::Testing::Declarative
 
-  test 'when authenticated should contain login, incomes, expenses, budgets and members links' do
+  private
+  def test_that_menu_is_present_on(action)
     sign_in users(:user_with_wallet_1)
-    get :index
+    get action
     assert_select 'a', 'user_with_wallet_1@budget.shellyapp.com'
     assert_select 'a', I18n.t('header.incomes')
     assert_select 'a', I18n.t('header.expenses')
@@ -11,16 +12,16 @@ module LayoutTests
     assert_select 'a', I18n.t('header.members')
   end
 
-  test 'should be message with actual balance' do
+  def test_that_should_contain_message_with_actual_balance_on(action)
     sign_in users(:user_with_wallet_2)
-    get :index
+    get action
     assert_select 'ul.pull-right' do
       assert_select 'a', "#{I18n.t('header.balance')}: #{number_to_currency(800)}"
     end
   end
 
-  test 'footer should contain addthis buttons' do
-    get :index
+  def test_that_footer_should_contain_add_this_buttons(action)
+    get action
     assert_select 'a.addthis_button_facebook_like'
     assert_select 'a.addthis_button_tweet'
     assert_select 'a.addthis_button_pinterest_pinit'
