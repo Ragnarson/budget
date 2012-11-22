9381a8d7a06514c01ff9bceaa83e1e6748cabfdb
Dawid Milewski <dawidmilewski89@gmail.com>
2012-11-22 13:15:52 +0100

    Changed total balance to use taxed income with some tests

--- app/models/balance.rb
+++ app/models/balance.rb
@@ -1,6 +1,6 @@
 class Balance
   def  self.actual(user)
-    total_incomes = user.incomes_sum
+    total_incomes = user.net_profits_sum
     total_expenses = user.expenses_sum
     total_incomes-total_expenses
   end
--- test/unit/balance_test.rb
+++ test/unit/balance_test.rb
@@ -1,4 +1,38 @@
 require 'test_helper'
 
 class BalanceTest < ActiveSupport::TestCase
+
+  def setup
+    @user = User.new
+  end
+
+  test "balance should be euqal zero if there is no income nor expenses" do
+    assert_equal Balance.actual(@user), 0
+  end
+
+  test "should count actual balance properly when there is no income" do
+    @user.expenses.build(amount: 100)
+    assert_equal Balance.actual(@user), -100
+  end
+
+  test "should count actual balance properly when there are no expenses" do
+    @user.incomes.build(amount: 100, tax: 10)
+    @user.incomes.build(amount: 100, tax: 0)
+    assert_equal Balance.actual(@user), 190
+  end
+
+  test "should count actual balance properly" do
+    @user.incomes.build(amount: 100)
+    @user.incomes.build(amount: 100)
+    @user.expenses.build(amount: 100)
+    assert_equal Balance.actual(@user), 100
+  end
+
+  test "should count actual balance wit tax properly" do
+    @user.incomes.build(amount: 100, tax: 10)
+    @user.incomes.build(amount: 100, tax: 0)
+    @user.expenses.build(amount: 100)
+    assert_equal Balance.actual(@user), 90
+  end
+
 end
