94b8127fdb8c7a7433b8d9210cda2b6d8f2d8f03
Dawid Milewski <dawidmilewski89@gmail.com>
2012-11-26 13:44:24 +0100

    Fixed main button tests to use I18n

--- test/integration/not_logged_in_test.rb
+++ test/integration/not_logged_in_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class NotLoggedInTest < ActionDispatch::IntegrationTest
+
+  test "should get homepage with login button in english" do
+    get "/en"
+    assert_select "a.btn", I18n.t('home.login')
+  end
   
+  test "should get homepage with login button in polish" do
+    get "/pl"
+    assert_select "a.btn", I18n.t('home.login')
+  end
+
   test "should respond with success when non-logged in user is accessing english homepage" do
     get "/en/"
     assert_response :success 
@@ -10,6 +20,11 @@ class NotLoggedInTest < ActionDispatch::IntegrationTest
   test "should respond with success when non-logged in user is accessing polish homepage" do
     get "/pl/"
     assert_response :success 
+  end
+
+  test "should show error 404 site if page doesn't exists" do
+    get "/pl/siema"
+    assert_template "404"
   end
 
 end
