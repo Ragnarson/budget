4cd3f8cddc6713f5d0f444edf62695ad67a7beaf
cintrzyk <marcin.brzezinski.inf@gmail.com>
2012-11-27 12:23:43 +0100

    Language will be set from user locale.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :get_actual_balance
+  before_filter :get_actual_balance, :set_locale
 
   def after_sign_in_path_for(resource)
     if current_user.locale.nil?
@@ -13,6 +13,12 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def set_locale
+    if current_user
+      I18n.locale = current_user.locale if current_user.locale?  
+    end
+  end
 
   def default_url_options(options={})
     {locale: I18n.locale}
