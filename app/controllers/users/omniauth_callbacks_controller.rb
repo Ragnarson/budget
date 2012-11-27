81306d7b5d7d1d358cbe0e07ec6cebc4a0ed4de2
cintrzyk <marcin.brzezinski.inf@gmail.com>
2012-11-27 20:37:33 +0100

    Added default language for new users.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  skip_before_filter :get_actual_balance
+  skip_before_filter :get_actual_balance, :set_locale
 
   def google_oauth2
     @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,8 @@ class User < ActiveRecord::Base
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
     data = access_token.info
     User.where(:email => data["email"]).first_or_create(email: data["email"],
-                                                        password: Devise.friendly_token[0,20])
+                                                        password: Devise.friendly_token[0,20],
+                                                        locale: "pl")
   end
 
   def self.generate_password(length = 8)
