a4732ea1d60c6bd82b1f4060993136b1a1bccc2c
Maciej Małecki <smt116@gmail.com>
2012-11-28 17:28:07 +0100

    Integration test for calculating total amount in adding new wallet.

---
+++ test/integration/planning_wallet_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class PlanningWalletTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:user_with_locale_pl)
+  end
+
+  test "that total amount should be calculated from expense amount" do
+    allow_google_login_as(@user)
+
+    visit '/pl'
+    click_on I18n.t('home.login', locale: 'pl')
+
+    visit '/pl/wallets/new'
+    click_on I18n.t('plan_wallet', locale: 'pl')
+
+    assert '0', find('#total_amount').text
+    find('input.currency').set('100')
+    assert '100.00', find('#total_amount').text
+  end
+end
