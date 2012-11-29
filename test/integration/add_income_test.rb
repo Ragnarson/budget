5f671e395506a13f4d062b1b50280e7b1f0f1a71
Dawid Milewski <dawidmilewski89@gmail.com>
2012-11-29 21:28:29 +0100

    Added integration test for adding first income

---
+++ test/integration/add_income_test.rb
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+require 'test_helper'
+
+class AddIncomeTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:user_with_locale_pl)
+  end
+
+  test "should add proper first income to incomes index" do
+    allow_google_login_as(@user)
+    
+    visit '/pl'
+    log_in_pl
+    click_link("Przychody")
+    click_link("dodaj przychód")
+    fill_in('income_source', with: "source")
+    fill_in('income_amount', with: 1000)
+    fill_in('income_tax', with: 0)
+    find('.form-actions input').click
+
+    assert_equal find('tbody tr td').text, "source"
+    assert_equal find('tbody tr td:nth-child(2)').text, "1 000,00 zł"
+    assert_equal find('tbody tr td:nth-child(3)').text, "0%"
+    assert_equal find('tbody tr td:nth-child(4)').text, "1 000,00 zł"
+    assert_equal find('tbody tr td:nth-child(5)').text, Time.now.strftime("%d.%m.%Y")
+
+  end
+
+  private
+  def log_in_pl
+    click_on I18n.t('home.login', locale: 'pl')
+  end
+end
