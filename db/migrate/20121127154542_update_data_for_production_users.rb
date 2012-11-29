d23320f6b3e083dbb80966e9c9a0693a5c797a9b
cintrzyk <marcin.brzezinski.inf@gmail.com>
2012-11-29 14:46:19 +0100

    Fixed migration 'update_data_for_production_users'

--- db/migrate/20121127154542_update_data_for_production_users.rb
+++ db/migrate/20121127154542_update_data_for_production_users.rb
@@ -1,39 +1,41 @@
 class UpdateDataForProductionUsers < ActiveRecord::Migration
   def up
-    User.all.each do |user|
-      if user.families.empty?
-        if user.invited_by
-          @family = FamiliesUsers.where(:user_id => user.invited_by).first
-          if !@family.empty?
-            @family = Family.create()
-            @family_id = @family.id
+    transaction do
+      User.all.each do |user|
+        if user.families.empty?
+          if user.invited_by
+            @family = FamiliesUsers.where(:user_id => user.invited_by).first
+            if @family.nil?
+              @family = Family.create()
+              @family_id = @family.id
+            else
+              @family_id = @family.family_id
+            end
+            @user_family = FamiliesUsers.create(:family_id => @family_id, :user_id => user.id)
+            @user_family.save
           else
-            @family_id = @family.family_id
+            @family = Family.create()
+            @family.save
+            @family_id = @family.id
+            @user_family = FamiliesUsers.create(:family_id => @family_id, :user_id => user.id)
+            @user_family.save
           end
-          @user_family = FamiliesUsers.create(:family_id => @family_id, :user_id => user.id)
-          @user_family.save
         else
-          @family = Family.create()
-          @family.save
-          @family_id = @family.id
-          @user_family = FamiliesUsers.create(:family_id => @family_id, :user_id => user.id)
-          @user_family.save
+          @family_id = user.families.first.id
         end
-      else
-        @family_id = user.families.first.id
-      end
 
-      @wallets = Wallet.where(:user_id => user.id)
-      if !@wallets.empty?
-        @wallets.each do |w|
-          w.update_attribute :family_id, @family_id
+        @wallets = Wallet.where(:user_id => user.id)
+        if !@wallets.empty?
+          @wallets.each do |w|
+            w.update_attribute :family_id, @family_id
+          end
         end
-      end
 
-      @incomes = Income.where(:user_id => user.id)
-      if !@incomes.empty?
-        @incomes.each do |i|
-          i.update_attribute :family_id, @family_id
+        @incomes = Income.where(:user_id => user.id)
+        if !@incomes.empty?
+          @incomes.each do |i|
+            i.update_attribute :family_id, @family_id
+          end
         end
       end
     end
