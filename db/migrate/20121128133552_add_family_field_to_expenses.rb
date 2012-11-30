c1e6c15d8cba9ed9648ac0598976cabf1ec721b9
Rafath Khan <rafath@zaraz.pl>
2012-11-30 07:03:04 +0100

    changed condition in migration for shellyapp

--- db/migrate/20121128133552_add_family_field_to_expenses.rb
+++ db/migrate/20121128133552_add_family_field_to_expenses.rb
@@ -6,7 +6,7 @@ class AddFamilyFieldToExpenses < ActiveRecord::Migration
     add_index :expenses, :user_id
 
     Expense.all.each do |expense|
-      expense.update_attributes({family_id: expense.wallet.family_id, user_id: expense.wallet.user_id}) if expense.wallet_id > 0
+      expense.update_attributes({family_id: expense.wallet.family_id, user_id: expense.wallet.user_id}) if expense.wallet_id.to_i > 0
     end
   end
 end
