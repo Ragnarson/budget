2c995483d3bc8bd4807f7e02aaa7df30cba507f0
Karolina Kruk <karolina.anna.kruk@gmail.com>
2012-12-03 13:07:22 +0100

    Removed infinite scroll on balance view

--- app/assets/javascripts/balance/balance_infinite_scroll.js.coffee
+++
@@ -1,7 +0,0 @@
-jQuery ->
-  if $('.pagination').length
-    $(window).scroll ->
-      url = $('.pagination .next_page a').attr('href')
-      if url &&  $(window).scrollTop() > $(document).height() - $(window).height() - 50
-        $.getScript(url)
-  $(window).scroll()
--- app/controllers/balance_controller.rb
+++ app/controllers/balance_controller.rb
@@ -4,6 +4,6 @@ class BalanceController < ApplicationController
   def index
     @total_incomes = current_user.families.first.incomes_sum
     @total_expenses = current_user.families.first.expenses_sum
-    @balances = Balance.history(current_user.families.first, params[:page], 10)
+    @balances = Balance.history(current_user.families.first, params[:page], 50)
   end
 end
