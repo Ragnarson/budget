5671b750afbb0785c76ce4c365adca0f914df1b6
Maciej Małecki <smt116@gmail.com>
2012-12-13 19:51:41 +0100

    Fixed integration test for editing user profile (race condition). Added asserts for proper translations on page (not only path).

--- test/integration/edit_profile_test.rb
+++ test/integration/edit_profile_test.rb
@@ -13,9 +13,16 @@ class EditProfileTest < ActionDispatch::IntegrationTest
 
     click_on('user_menu')
     click_on I18n.t('header.edit_profile', locale: 'pl')
+
+    assert find('ul.nav ul li').has_no_content?(I18n.t('header.edit_profile', locale: 'en'))
+    assert find('ul.nav ul li').has_content?(I18n.t('header.edit_profile', locale: 'pl'))
+    assert_equal current_path, '/pl/edit'
+
     select('English', from: 'user_locale')
     click_on I18n.t('helpers.submit.update', locale: 'pl')
 
+    assert find('ul.nav ul li').has_no_content?(I18n.t('header.edit_profile', locale: 'pl'))
+    assert find('ul.nav ul li').has_content?(I18n.t('header.edit_profile', locale: 'en'))
     assert_equal current_path, '/en/edit'
   end
 end
